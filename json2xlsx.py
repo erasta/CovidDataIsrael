3d2328474682d25ba17c3c048ed1076cd73bcbaf
erasta <eran.geva@gmail.com>
2020-09-22 18:58:31 +0300

    col width

--- json2xlsx.py
+++ json2xlsx.py
@@ -1,7 +1,51 @@
 import json
 import os
+from typing import Optional
 
 from xlsxwriter import Workbook
+from xlsxwriter.worksheet import (Worksheet, cell_number_tuple,
+                                  cell_string_tuple)
+
+
+def get_column_width(worksheet: Worksheet, column: int) -> Optional[int]:
+    """Get the max column width in a `Worksheet` column."""
+    strings = getattr(worksheet, '_ts_all_strings', None)
+    if strings is None:
+        strings = worksheet._ts_all_strings = sorted(
+            worksheet.str_table.string_table,
+            key=worksheet.str_table.string_table.__getitem__)
+    lengths = set()
+    for row_id, colums_dict in worksheet.table.items():  # type: int, dict
+        data = colums_dict.get(column)
+        if not data:
+            continue
+        if type(data) is cell_string_tuple:
+            iter_length = len(strings[data.string])
+            if not iter_length:
+                continue
+            lengths.add(iter_length)
+            continue
+        if type(data) is cell_number_tuple:
+            iter_length = len(str(data.number))
+            if not iter_length:
+                continue
+            lengths.add(iter_length)
+    if not lengths:
+        return None
+    return max(lengths)
+
+
+def set_column_autowidth(worksheet: Worksheet, column: int):
+    """
+    Set the width automatically on a column in the `Worksheet`.
+    !!! Make sure you run this function AFTER having all cells filled in
+    the worksheet!
+    """
+    maxwidth = get_column_width(worksheet=worksheet, column=column)
+    if maxwidth is None:
+        return
+    worksheet.set_column(first_col=column, last_col=column, width=maxwidth)
+
 
 os.makedirs('out', exist_ok=True)
 names = []
@@ -13,7 +57,7 @@ with Workbook('out/covid.xlsx') as workbook:
         fields = list(data[0].keys())
         print(i, fields)
 
-        shname = '_'.join(list(filter(lambda x:x != 'date', fields))[0:4])
+        shname = '_'.join(list(filter(lambda x: x != 'date', fields))[0:4])
         if shname == 'name_amount':
             shname = '_'.join(map(lambda x: x['name'], data))
         shname = shname[:29]
@@ -26,3 +70,6 @@ with Workbook('out/covid.xlsx') as workbook:
         for index, item in enumerate(data):
             row = map(lambda field_id: item.get(field_id, ''), fields)
             worksheet.write_row(row=index + 1, col=0, data=row)
+
+        for i, _ in enumerate(fields):
+            set_column_autowidth(worksheet, i)
