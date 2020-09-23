cc1d684e09352657a71034e67022109621a20302
erasta <eran.geva@gmail.com>
2020-09-23 12:12:02 +0300

    organize sheet names

--- json2xlsx.py
+++ json2xlsx.py
@@ -7,15 +7,25 @@ from dashreq import get_dash_data, get_dash_req
 from xlscolumn import worksheet_autowidth
 
 dashrequest = get_dash_req()
-sheets = list(map(lambda x: x["queryName"], dashrequest["requests"]))
-sheets = [name + (str(sheets[:i].count(name)+1) if sheets.count(name) > 1 else '') for i, name in enumerate(sheets)]
+sheets = list(map(lambda x: x['queryName'], dashrequest['requests']))
+sheets = [name[0].lower() + name[1:] for name in sheets]
+uniqsheets = [x for i, x in enumerate(sheets) if i == sheets.index(x)]
+# datas = list(range(len(sheets)))
 
 dashjson = get_dash_data()
+datas = list(map(lambda x: x['data'], dashjson))
+
+datazip = []
+for u in uniqsheets:
+    if sheets.count(u) == 1:
+        datazip.append((u, datas[sheets.index(u)]))
+    else:
+        for occur, index in enumerate([i for i, x in enumerate(sheets) if x == u]):
+            datazip.append((u + str(occur + 1), datas[index]))
 
 os.makedirs('out', exist_ok=True)
 with Workbook('out/covid.xlsx') as workbook:
-    datas = map(lambda x: x['data'], dashjson)
-    for sheetname, data in zip(sheets, datas):
+    for sheetname, data in datazip:
 
         if not isinstance(data, list):
             data = [data]
