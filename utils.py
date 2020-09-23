26ce8afcd2e0b7eb83122090469108c107698939
erasta <eran.geva@gmail.com>
2020-09-23 18:23:18 +0300

    computeDelta

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import os
+import csv
 
 
 def group_sheet_data(sheets, datas):
@@ -12,6 +14,48 @@ def group_sheet_data(sheets, datas):
             for occur, index in enumerate([i for i, x in enumerate(sheets) if x == u]):
                 datazip.append((u + str(occur + 1), datas[index]))
     return datazip
+
+
+def getFieldByDate(rows, date, fieldname):
+    fit = [row for row in rows if row['date'] == date]
+    return fit[0][fieldname] if len(fit) > 0 else None
+
+
+def getRowByDate(rows, date):
+    fit = [row for row in rows if row['date'] == date]
+    return fit[0] if len(fit) > 0 else None
+
+
+def coupleDatasByDates(one, two):
+    dates = [row['date'] for row in one] + [row['date'] for row in two]
+    dates = sorted(list(set(dates)))
+    return [(d, getRowByDate(one, d), getRowByDate(two, d)) for d in dates]
+
+
+def computeDelta(data, csvname):
+    if not os.path.isfile(csvname):
+        return data
+    data = [x.copy() for x in data]
+    old = list(csv.DictReader(open(csvname)))
+    couple = coupleDatasByDates(data, old)
+    coupleAmounts = [(date,
+                      newrow['amount'] if newrow is not None else None,
+                      int(oldrow['amount']) if oldrow is not None else None)
+                     for date, newrow, oldrow in couple]
+    changes = [oldamount != newamount for date, newamount, oldamount in coupleAmounts[:-1]]
+    changesExist = any(changes)
+    newdate = old[-1]['date'] if changesExist else None
+
+    ret = []
+    for (date, newrow, oldrow), (_, newamount, oldamount) in zip(couple, coupleAmounts):
+        row = {'date': date, 'amount': newamount}
+        if changesExist:
+            row[newdate] = oldamount
+        for col in list(old[0].keys())[2:]:
+            row[col] = oldrow[col]
+        ret.append(row)
+
+    return ret
 
 
 def computeWeekly(data):
