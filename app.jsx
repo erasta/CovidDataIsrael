6fdbec7f863e137f1fff77fec8c1755c07aaf886
erasta <eran.geva@gmail.com>
2020-09-24 18:08:52 +0300

    CsvButtons

--- app.jsx
+++ app.jsx
@@ -7,28 +7,6 @@ sheetname = sheetname || 'contagionDataPerCityPublic';
 const fileshow = sheetname === 'all' ? 'out/covid.csv' : `out/csv/${sheetname}.csv`
 console.log(fileshow);
 
-const CsvLink = ({ name, downloadlink, showlink }) => {
-    const showname = name.replace(/(?:^|\.?)([A-Z])/g, function (x, y) { return "_" + y }).replace(/^_/, "").replace(/_/g, " ");
-    return <div style={{ margin: 3 }}>
-        <ButtonGroup disableElevation variant="contained" color="primary">
-            <Button href={showlink} disabled={!showlink}>{showname}</Button>&nbsp;
-                <Button href={downloadlink}><Icon>get_app</Icon></Button>
-        </ButtonGroup>
-    </div>;
-}
-
-const CsvButtons = ({ names }) => (
-    <div>
-        <CsvLink key={'xlsx'} name='Xls file' downloadlink='out/covid.xlsx' />
-        <CsvLink key={'all'} name='Csv containing all' downloadlink='out/covid.csv' showlink='?sheet=all' />
-        {
-            names.map(name => (
-                <CsvLink key={name} name={name} downloadlink={`out/csv/${name}.csv`} showlink={`?sheet=${name}`} />
-            ))
-        }
-    </div>
-)
-
 const checkAllKeysAreSame = (data) => {
     return data.length && data.find(d => Object.keys(d) === Object.keys(data[0]));
 }
