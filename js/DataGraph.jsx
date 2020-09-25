25ad3b7f565683c86ce52139f380717e4fa51e3f
erasta <eran.geva@gmail.com>
2020-09-25 20:24:06 +0300

    group by week month

--- js/DataGraph.jsx
+++ js/DataGraph.jsx
@@ -25,23 +25,65 @@ const extractDateAndNumbers = (parsed) => {
     return [numitems, numfields, dates];
 }
 
+const weekNum = (datequery) => {
+    let onejan = new Date(1990, 0, 1);
+    return Math.ceil((((datequery.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7);
+}
+
+const onlyUnique = (arr) => {
+    return arr.filter((x, i) => arr.indexOf(x) === i)
+}
+
+const groupByTime = (group, dates, nums) => {
+    if (group === 'Exact') {
+        return [dates, nums];
+    }
+    let weeknums;
+    if (group === 'Weekly') {
+        weeknums = dates.map(weekNum);
+    } else if (group === 'Monthly') {
+        weeknums = dates.map(d => d.getFullYear() * 10 + d.getMonth());
+    } else {
+        weeknums = dates.map(d => d.getFullYear() * 1000 + d.getMonth() * 10 + d.getDate());
+    }
+    const uniqWeeknums = onlyUnique(weeknums);
+    const groupdates = uniqWeeknums.map(w => {
+        const datesinweek = dates.filter((d, i) => weeknums[i] === w);
+        return new Date(Math.max(...datesinweek));
+    })
+    const groupnums = uniqWeeknums.map(w => nums.filter((d, i) => weeknums[i] === w).reduce((a, b) => a + b, 0))
+    return [groupdates, groupnums];
+};
+
+const groupGroupsByTime = (group, dates, numitems) => {
+    let groupdates;
+    console.log(group)
+    const groupnumitems = numitems.map(nums => {
+        let groupnums;
+        [groupdates, groupnums] = groupByTime(group, dates, nums);
+        return groupnums;
+    })
+    return [groupdates, groupnumitems];
+}
+
 const DataGraph = ({ parsed }) => {
     const [chartStyle, setChartStyle] = React.useState('Line');
+    const [timeGroup, setTimeGroup] = React.useState('Exact');
 
     const [numitems, numfields, dates] = extractDateAndNumbers(parsed);
-    console.log(numitems)
+    const [groupdates, groupnumitems] = groupGroupsByTime(timeGroup, dates, numitems);
     let data = {}
     if (numfields.length) {
         data = {
-            labels: dates.map(d => d.toLocaleDateString()),
-            datasets: numitems.map((field, i) => {
+            labels: groupdates.map(d => d.toLocaleDateString()),
+            datasets: groupnumitems.map((field, i) => {
                 const color = new Array(3).fill().map(() => '' + Math.floor(Math.random() * 256)).join(',')
                 return {
                     label: numfields[i],
                     backgroundColor: 'rgba(' + color + ',0.2)',
-                    borderColor: 'rgba(' + color + ',1)',
+                    borderColor: 'rgba(' + color + ',0.8)',
                     borderWidth: 1,
-                    hoverBackgroundColor: 'rgba(' + color + ',0.4)',
+                    hoverBackgroundColor: 'rgba(' + color + ',0.6)',
                     hoverBorderColor: 'rgba(' + color + ',1)',
                     data: field,
                 }
@@ -53,10 +95,19 @@ const DataGraph = ({ parsed }) => {
             <>
                 <Select
                     value={chartStyle}
-                    onChange={setChartStyle}
+                    onChange={e => setChartStyle(e.target.value)}
                 >
                     <MenuItem value={'Bar'} >Bars Chart</MenuItem>
                     <MenuItem value={'Line'} >Lines Chart </MenuItem>
+                </Select>
+                <Select
+                    value={timeGroup}
+                    onChange={e => setTimeGroup(e.target.value)}
+                >
+                    <MenuItem value={'Exact'} >No time groupings</MenuItem>
+                    <MenuItem value={'Daily'} >Daily</MenuItem>
+                    <MenuItem value={'Weekly'} >Weekly</MenuItem>
+                    <MenuItem value={'Monthly'} >Monthly</MenuItem>
                 </Select>
                 {(chartStyle === 'Line') ?
                     <ReactChartjs2.Line
