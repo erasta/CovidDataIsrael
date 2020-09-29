35848969294c0069bb4990b83b0934a7aa838377
erasta <eran.geva@gmail.com>
2020-09-29 15:37:05 +0300

    mute all button

--- js/FieldChips.jsx
+++ js/FieldChips.jsx
@@ -1,8 +1,19 @@
 const { Grid, Chip } = MaterialUI;
 
 const FieldChips = ({ fieldNames, colors, mutedFields, setMutedFields }) => {
+    console.log(mutedFields);
     return (
         <Grid container justify="center">
+            {
+                fieldNames.length <= 1 ? null : <Chip
+                    icon={<Icon >highlight_off</Icon>}
+                    key={'?muteall'}
+                    size="small"
+                    label={mutedFields.length < fieldNames.length ? 'mute all' : 'show all'}
+                    clickable
+                    onClick={() => { setMutedFields(mutedFields.length < fieldNames.length ? fieldNames : []) }}
+                />
+            }
             {
                 fieldNames.map((field, i) =>
                     <Chip
