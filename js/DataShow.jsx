5e1b2349d35a56fff97311fca8bdb18a5bf5de50
erasta <eran.geva@gmail.com>
2020-10-01 12:28:45 +0300

    TableShow.jsx

--- js/DataShow.jsx
+++ js/DataShow.jsx
@@ -12,60 +12,6 @@ const {
     Link
 } = MaterialUI;
 
-const TableShow = ({ parsed }) => {
-    const [order, setOrder] = React.useState({ by: null, asc: 'asc' });
-    React.useEffect(() => {
-        if (parsed && parsed.length && parsed[0].hasOwnProperty('date')) {
-            setOrder({ by: 'date', asc: 'desc' });
-        }
-    }, [parsed])
-    const columns = parsed.length ? Object.keys(parsed[0]) : [];
-    const rows = sortBy(parsed, order.by, order.asc === 'asc');
-    return (
-        <Paper >
-            <TableContainer style={{ maxHeight: 1000 }}>
-                <Table stickyHeader size="small">
-                    <TableHead>
-                        <TableRow>
-                            {columns.map((column, i) => (
-                                <TableCell
-                                    style={{ backgroundColor: 'lightgrey' }}
-                                    key={i}
-                                >
-                                    <TableSortLabel
-                                        active={order.by === column}
-                                        direction={order.by === column ? order.asc : 'asc'}
-                                        onClick={(event) => {
-                                            const property = event.target.textContent;
-                                            const isAsc = order.by === property && order.asc === 'asc';
-                                            setOrder({ by: property, asc: isAsc ? 'desc' : 'asc' })
-                                        }}
-                                    >
-                                        {column}
-                                    </TableSortLabel>
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {rows.map((row, ridx) => (
-                            <TableRow
-                                key={ridx}
-                            >
-                                {columns.map((column, cidx) => (
-                                    <TableCell key={cidx}>
-                                        {convertToShow(row[column])}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </TableContainer>
-        </Paper>
-    )
-}
-
 const computeForTable = (name, data) => {
     if (name === 'testResultsPerDate') {
         data.forEach(row => {
---
+++ js/TableShow.jsx
@@ -0,0 +1,67 @@
+const {
+    Paper,
+    TableContainer,
+    Table,
+    TableHead,
+    TableRow,
+    TableBody,
+    TableCell,
+    TableSortLabel,
+    CardContent,
+    Typography,
+    Link
+} = MaterialUI;
+
+const TableShow = ({ parsed }) => {
+    const [order, setOrder] = React.useState({ by: null, asc: 'asc' });
+    React.useEffect(() => {
+        if (parsed && parsed.length && parsed[0].hasOwnProperty('date')) {
+            setOrder({ by: 'date', asc: 'desc' });
+        }
+    }, [parsed])
+    const columns = parsed.length ? Object.keys(parsed[0]) : [];
+    const rows = sortBy(parsed, order.by, order.asc === 'asc');
+    return (
+        <Paper >
+            <TableContainer style={{ maxHeight: 1000 }}>
+                <Table stickyHeader size="small">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column, i) => (
+                                <TableCell
+                                    style={{ backgroundColor: 'lightgrey' }}
+                                    key={i}
+                                >
+                                    <TableSortLabel
+                                        active={order.by === column}
+                                        direction={order.by === column ? order.asc : 'asc'}
+                                        onClick={(event) => {
+                                            const property = event.target.textContent;
+                                            const isAsc = order.by === property && order.asc === 'asc';
+                                            setOrder({ by: property, asc: isAsc ? 'desc' : 'asc' })
+                                        }}
+                                    >
+                                        {column}
+                                    </TableSortLabel>
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row, ridx) => (
+                            <TableRow
+                                key={ridx}
+                            >
+                                {columns.map((column, cidx) => (
+                                    <TableCell key={cidx}>
+                                        {convertToShow(row[column])}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Paper>
+    )
+}
