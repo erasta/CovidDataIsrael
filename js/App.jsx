4e7fad2e5f5eef9eb4d180c495107e5d0bdd731f
Eran Geva <eran.geva@gmail.com>
2020-10-03 10:30:33 +0300

    Header

--- js/App.jsx
+++ js/App.jsx
@@ -32,36 +32,7 @@ const App = ({ names, languages }) => {
 
     return (
         <>
-            <Grid container direction="row">
-                <Grid item xs={3}>
-                    <Grid container direction="row" justify="flex-start" alignItems="center">
-                        <IconButton onClick={() => setLanguage(language === 'he' ? 'en' : 'he')}>
-                            <img width={32} height={32} src={`images/${language === 'he' ? 'il' : 'gb'}.svg`}></img>
-                        </IconButton>
-                        <LastUpdate lang={lang} />
-                    </Grid>
-                </Grid>
-                <Grid item xs={6}>
-                    <h1 style={{
-                        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-                        textAlign: 'center',
-                        fontSize: 'xx-large'
-                    }}>
-                        {trans(lang, 'Covid-19 Data Israel')}
-                    </h1>
-                </Grid>
-                <Grid item xs={3}>
-                    <MaterialUI.Link href="https://eran.dev/" style={{ textDecoration: 'none' }} target="_blank" >
-                        <p style={{
-                            fontFamily: 'Source Sans Pro, sans-serif',
-                            textAlign: 'right',
-                            marginRight: 10
-                        }}>
-                            {trans(lang, 'Contact')}
-                        </p>
-                    </MaterialUI.Link>
-                </Grid>
-            </Grid>
+            <Header language={language} setLanguage={setLanguage} lang={lang} />
             <MainView name={name} names={names} lang={lang} />
             <Footer />
         </>
