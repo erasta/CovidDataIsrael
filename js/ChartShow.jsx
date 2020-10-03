7de112d3f7098bbd3e2858d60c4c053c203a67dd
Eran Geva <eran.geva@gmail.com>
2020-10-03 15:59:25 +0300

    chartshow.jsx

---
+++ js/ChartShow.jsx
@@ -0,0 +1,61 @@
+var image = new Image();
+image.src = "images/eran.dev.water.png";
+
+const ChartShow = ({ chartStyle, dates, fieldNames, mutedFields, fieldValues, fromDate, toDateInc }) => {
+    if (!dates.length || !fieldNames.length) return null;
+
+    const data = {
+        labels: dates,
+        datasets: fieldValues.map((field, i) => {
+            const color = colorByNumber(i, fieldNames.length + 1);
+            return {
+                type: chartStyle,
+                label: fieldNames[i],
+                backgroundColor: attachAlpha(color, 0.2),
+                borderColor: attachAlpha(color, 1),
+                borderWidth: 1,
+                pointRadius: 1,
+                data: field,
+                hidden: mutedFields.includes(fieldNames[i])
+            }
+        })
+    };
+
+    return (
+        <ReactChartjs2.default
+            legend={false}
+            data={data}
+            type={chartStyle}
+            options={{
+                scales: {
+                    yAxes: [
+                        {
+                            ticks: {
+                                beginAtZero: 0,
+                                min: 0,
+                            }
+                        }
+                    ],
+                    xAxes: [
+                        {
+                            ticks: {
+                                min: fromDate,
+                                max: toDateInc
+                            },
+                            type: 'time',
+                            time: {
+                                displayFormats: {
+                                    day: 'D/M'
+                                }
+                            }
+                        }
+                    ]
+                },
+                watermark: {
+                    image: image, opacity: 0.07, alignToChartArea: true, width: 50, height: 20
+                }
+            }}
+        />
+    )
+}
+
--- js/DataGraph.jsx
+++ js/DataGraph.jsx
@@ -105,67 +105,6 @@ const dateByPercent = (dates, percent) => {
     return dates[Math.round(percent / 100 * (dates.length - 1))];
 }
 
-var image = new Image();
-image.src = "images/eran.dev.water.png";
-
-const ChartShow = ({ chartStyle, dates, fieldNames, mutedFields, fieldValues, fromDate, toDateInc }) => {
-    if (!dates.length || !fieldNames.length) return null;
-
-    const data = {
-        labels: dates,
-        datasets: fieldValues.map((field, i) => {
-            const color = colorByNumber(i, fieldNames.length + 1);
-            return {
-                type: chartStyle,
-                label: fieldNames[i],
-                backgroundColor: attachAlpha(color, 0.2),
-                borderColor: attachAlpha(color, 1),
-                borderWidth: 1,
-                pointRadius: 1,
-                data: field,
-                hidden: mutedFields.includes(fieldNames[i])
-            }
-        })
-    };
-
-    return (
-        <ReactChartjs2.default
-            legend={false}
-            data={data}
-            type={chartStyle}
-            options={{
-                scales: {
-                    yAxes: [
-                        {
-                            ticks: {
-                                beginAtZero: 0,
-                                min: 0,
-                            }
-                        }
-                    ],
-                    xAxes: [
-                        {
-                            ticks: {
-                                min: fromDate,
-                                max: toDateInc
-                            },
-                            type: 'time',
-                            time: {
-                                displayFormats: {
-                                    day: 'D/M'
-                                }
-                            }
-                        }
-                    ]
-                },
-                watermark: {
-                    image: image, opacity: 0.07, alignToChartArea: true, width: 50, height: 20
-                }
-            }}
-        />
-    )
-}
-
 const DataGraph = ({ parsed, showControls }) => {
     const [chartStyle, setChartStyle] = React.useState(localStorage.getItem('chartStyle') || 'line');
     const [timeGroup, setTimeGroup] = React.useState('Exact');
