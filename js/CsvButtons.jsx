72ef1eb11381ea2b4301e5cab4b05b4471320c0e
Eran Geva <eran.geva@gmail.com>
2020-10-05 19:40:32 +0300

    remove download link

--- js/CsvButtons.jsx
+++ js/CsvButtons.jsx
@@ -2,17 +2,13 @@ const {
     ButtonGroup, Button, Icon, Grid, IconButton, CircularProgress
 } = MaterialUI;
 
-const CsvLink = ({ name, downloadlink, showlink, lang }) => {
-    // const showname = camelCaseToSnake(name).replace(/_/g, " ");
-    return <div style={{ margin: 3 }}>
+const CsvLink = ({ name, showlink, lang }) => (
+    <div style={{ margin: 3 }}>
         <ButtonGroup disableElevation variant="contained" color="primary">
             <Button component={Link} to={showlink} disabled={!showlink}>{trans(lang, name)}</Button>
-            {downloadlink ?
-                <Button href={downloadlink}><Icon>get_app</Icon></Button>
-                : null}
         </ButtonGroup>
-    </div>;
-}
+    </div>
+)
 
 const CsvButtons = ({ names, lang }) => (
     <div>
@@ -20,7 +16,7 @@ const CsvButtons = ({ names, lang }) => (
         <CsvLink key={'infectedVsDead'} name='infectedVsDead' showlink='?sheet=infectedVsDead' lang={lang} />
         {
             names.map(name => (
-                <CsvLink key={name} name={name} downloadlink={`out/csv/${name}.csv`} showlink={`?sheet=${name}`} lang={lang} />
+                <CsvLink key={name} name={name} showlink={`?sheet=${name}`} lang={lang} />
             ))
         }
     </div>
