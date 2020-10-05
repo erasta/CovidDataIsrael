fa9fb25e7437519b2d25109ba8a297c09e5d5f10
Eran Geva <eran.geva@gmail.com>
2020-10-05 23:22:22 +0300

    reorg

--- js/MainView.jsx
+++ js/MainView.jsx
@@ -1,8 +1,32 @@
+const {
+    ButtonGroup, Button, Icon, Grid, IconButton, CircularProgress
+} = MaterialUI;
+
 const ShowByName = ({ name, names, lang }) => {
     if (name === 'showcharts') return <DataShowCharts names={names} lang={lang} />
     if (name === 'infectedVsDead') return <DataShowComputedDeath showtable={true} lang={lang} />
     return <DataShow name={name} lang={lang} />
 }
+
+const CsvButtons = ({ names, lang }) => (
+    <Grid container direction='column'>
+        {
+            ['ShowCharts', 'infectedVsDead'].concat(names).map(name => (
+                <Button
+                    key={name}
+                    component={Link}
+                    to={`?sheet=${name}`}
+                    style={{ margin: 3 }}
+                    variant="contained"
+                    color="primary"
+                >
+                    {trans(lang, name)}
+                </Button>
+            ))
+        }
+    </Grid>
+)
+
 
 const MainView = ({ names, name, lang }) => (
     <Grid container direction="row">
