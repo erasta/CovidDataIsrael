8b0d75f6eec2007fbd5c18a207c367ab0ff60071
Eran Geva <eran.geva@gmail.com>
2020-10-13 12:59:47 +0300

    play history on map

--- js/DataMap.jsx
+++ js/DataMap.jsx
@@ -2,18 +2,25 @@ const { Map: LeafletMap, TileLayer, Marker, Popup, Circle, CircleMarker } = wind
 
 const DataMap = ({ height = 800 }) => {
     const [cities, setCities] = React.useState([]);
+    const [geoloc, setGeoloc] = React.useState([]);
+    const [showHistory, setShowHistory] = React.useState(false);
     React.useEffect(() => {
         (async () => {
-            let parsed = await fetchTable('contagionDataPerCityPublic', tableFileName('contagionDataPerCityPublic'));
-            const geoloc = await fetchCsv('jsons/city_geoloc.csv');
+            const geoloc1 = await fetchCsv('jsons/city_geoloc.csv');
+            setGeoloc(geoloc1);
+        })();
+    }, [])
+    React.useEffect(() => {
+        (async () => {
+            let parsed = await fetchTable('contagionDataPerCityPublic', tableFileName('contagionDataPerCityPublic', showHistory));
             parsed.forEach(city => {
                 const loc = geoloc.find(c => c.SettlementCode === city['City Code']);
                 city.latlng = !loc ? undefined : [loc.Y_GEO, loc.X_GEO];
             });
-            parsed = parsed.filter(city => city.latlng);
+            parsed = parsed.filter(city => city.latlng && !isNaN(city.latlng[0]) && !isNaN(city.latlng[1]));
             setCities(parsed);
         })();
-    }, [])
+    }, [showHistory, geoloc])
     const fields = [
         { name: 'Verified/Tests ratio', color: 'red' },
         { name: 'Infected Per 10000', color: 'blue' },
@@ -23,54 +30,63 @@ const DataMap = ({ height = 800 }) => {
     ];
 
     return (
-        <LeafletMap center={[32.0897, 34.8042]} zoom={12} style={{ height: height }}>
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {
-                fields.map((field, i) => {
-                    const maxval = Math.max(...cities.map(city => city[field.name]));
-                    return cities.map(city => (
-                        <SemiCircle
-                            position={city.latlng}
-                            key={city['City Code']}
-                            radius={city[field.name] * 2500 / maxval}
-                            weight={1}
-                            color={field.color}
-                            startAngle={i * 360 / fields.length}
-                            stopAngle={(i + 1) * 360 / fields.length}
-                            fillOpacity={0.4}
-                            onMouseOver={(e) => {
-                                e.target.openPopup();
-                            }}
-                            onMouseOut={(e) => {
-                                e.target.closePopup();
-                            }}
-                        >
-                            <Popup>
-                                <p key='city' style={{ margin: 0, textAlign: 'right', fontWeight: 600 }}>
-                                    {city.City}
-                                </p>
-                                <table style={{ border: 'none' }}>
-                                    <tbody>
-                                        {fields.map(f => (
-                                            <tr key={f.name} style={{ border: 'none', margin: 0, color: f.color, whiteSpace: 'nowrap' }}>
-                                                <td key={1} style={{ border: 'none' }}>
-                                                    {f.name + (f.name === 'Verified/Tests ratio' ? ' Last 7 Days' : '')}
-                                                </td>
-                                                <td key={2} style={{ border: 'none', textAlign: 'right' }}>
-                                                    {truncateDigits(city[f.name])}
-                                                </td>
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </Popup>
-                        </SemiCircle>
-                    ))
-                })
-            }
-        </LeafletMap>
+        <>
+            <HistorySlider onHistory={v => setShowHistory(v)} withPlay={true} />
+            <LeafletMap center={[32.0897, 34.8042]} zoom={12} style={{ height: height }}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {
+                    fields.map((field, i) => {
+                        const maxval = Math.max(...cities.map(city => city[field.name]));
+                        return cities.map(city => {
+                            // console.log(city.City, city['City Code'], city.latlng)
+                            if (!city[field.name]) {
+                                return null;
+                            }
+                            return (
+                                <SemiCircle
+                                    position={city.latlng}
+                                    key={city['City Code']}
+                                    radius={city[field.name] * 2500 / maxval}
+                                    weight={1}
+                                    color={field.color}
+                                    startAngle={i * 360 / fields.length}
+                                    stopAngle={(i + 1) * 360 / fields.length}
+                                    fillOpacity={0.4}
+                                    onMouseOver={(e) => {
+                                        e.target.openPopup();
+                                    }}
+                                    onMouseOut={(e) => {
+                                        e.target.closePopup();
+                                    }}
+                                >
+                                    <Popup>
+                                        <p key='city' style={{ margin: 0, textAlign: 'right', fontWeight: 600 }}>
+                                            {city.City}
+                                        </p>
+                                        <table style={{ border: 'none' }}>
+                                            <tbody>
+                                                {fields.map(f => (
+                                                    <tr key={f.name} style={{ border: 'none', margin: 0, color: f.color, whiteSpace: 'nowrap' }}>
+                                                        <td key={1} style={{ border: 'none' }}>
+                                                            {f.name + (f.name === 'Verified/Tests ratio' ? ' Last 7 Days' : '')}
+                                                        </td>
+                                                        <td key={2} style={{ border: 'none', textAlign: 'right' }}>
+                                                            {truncateDigits(city[f.name])}
+                                                        </td>
+                                                    </tr>
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </Popup>
+                                </SemiCircle>
+                            )
+                        })
+                    })
+                }
+            </LeafletMap>
+        </>
     )
 }
--- js/HistorySlider.jsx
+++ js/HistorySlider.jsx
@@ -2,41 +2,62 @@ const {
     MenuItem, Select, InputLabel, FormHelperText, FormControlLabel, Switch, Checkbox, Typography, Slider
 } = MaterialUI;
 
-const HistorySlider = ({ onHistory }) => {
+const HistorySlider = ({ onHistory, withPlay = false }) => {
     const [dates, setDates] = React.useState([]);
     const [pos, setPos] = React.useState(100);
+    const [play, setPlay] = React.useState(false);
     React.useEffect(() => {
         (async () => {
             const data = await fetchFile('out/history/dates.json');
             setDates(data ? JSON.parse(data) : []);
         })();
     }, []);
+
+    if (play) {
+        setTimeout(() => {
+            if (pos === 100) {
+                setPos(0);
+            } else {
+                const newpos = pos + 100 / (dates.length - 1);
+                const d = dates[Math.round(newpos / 100 * (dates.length - 1))];
+                onHistory(d);
+                setPos(newpos);
+            }
+        }, 500);
+    }
     return (
-        <Grid container
+        <Grid container direction="row" justify="center" alignItems="center"
             spacing={2}
             style={{
                 marginLeft: '5%',
                 width: '90%'
             }}>
+            {!withPlay ? null :
+                <Grid item>
+                    <IconButton>
+                        <Icon onClick={() => setPlay(!play)}> play_arrow</Icon>
+                    </IconButton>
+                </Grid>
+            }
             <Grid item>
                 <Icon>history</Icon>
             </Grid>
             <Grid item xs>
                 <Slider
-                disabled={!dates.length}
-                value={pos}
-                onChange={(e, v) => {
-                    const d = v === 100 ? false : dates[Math.round(v / 100 * (dates.length - 1))];
-                    onHistory(d);
-                    setPos(v);
-                }}
-                step={dates.length ? 100 / dates.length : undefined}
-                valueLabelDisplay="auto"
-                valueLabelFormat={val => {
-                    const d = new Date(dates[Math.round(val / 100 * (dates.length - 1))])
-                    return d.getDate() + '.' + (d.getMonth() + 1)
-                }}
-            />
+                    disabled={!dates.length}
+                    value={pos}
+                    onChange={(e, v) => {
+                        const d = v === 100 ? false : dates[Math.round(v / 100 * (dates.length - 1))];
+                        onHistory(d);
+                        setPos(v);
+                    }}
+                    step={dates.length ? 100 / dates.length : undefined}
+                    valueLabelDisplay={play ? "on" : "auto"}
+                    valueLabelFormat={val => {
+                        const d = new Date(dates[Math.round(val / 100 * (dates.length - 1))])
+                        return d.getDate() + '.' + (d.getMonth() + 1)
+                    }}
+                />
             </Grid>
         </Grid>
     )
