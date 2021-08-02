d85ef18c0fbdc05591f3115e307aa216d6ddc5dc
Eran Geva <eran.geva@gmail.com>
2021-08-03 00:17:46 +0300

    fix day_date to date

--- js/FetchedTable.js
+++ js/FetchedTable.js
@@ -116,6 +116,7 @@ class FetchedTable {
         this.renameField(this.data, 'תאריך', 'date');
         this.renameField(this.data, 'Date', 'date');
         this.renameField(this.data, 'Day_Date', 'date');
+        this.renameField(this.data, 'day_date', 'date');
         if (this.data.length) {
             if (this.data[0].hasOwnProperty('date')) {
                 this.data.sort((a, b) => a.date.getTime() - b.date.getTime());
